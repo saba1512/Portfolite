5fa73334cd7eb151dbd4000a4ac7a9d3a09c4b02
saba1512 <khorguashvilisaba5@gmail.com>
2026-04-14 21:12:05 +0400

    commit

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,17 @@
 const burger = document.querySelector('.burger');
 const navBar = document.querySelector('.nav-bar');
+const navLinks = document.querySelectorAll('.nav-bar li a'); // ვიღებთ ყველა ლინკს მენიუში
 
+// ბურგერზე დაჭერისას მენიუს გახსნა/დაკეტვა
 burger.addEventListener('click', () => {
     navBar.classList.toggle('nav-active');
-    
     burger.classList.toggle('toggle');
+});
+
+// თითოეულ ლინკზე დაჭერისას მენიუს ავტომატური დაკეტვა
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navBar.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+    });
 });
